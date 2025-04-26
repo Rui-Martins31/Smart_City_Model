a7627adabd3d160c30b0ee897c437dd2140e47e1
Rui <up202104636@up.pt>
2025-04-26 13:01:25 +0100

    DATABASE: posting data to the endpoint

--- ESIST_DATABSE/connectToDatabase/connectToDatabase.ino
+++ ESIST_DATABSE/connectToDatabase/connectToDatabase.ino
@@ -1,8 +1,20 @@
 #include <WiFi.h>
+#include <WebServer.h>
 #include "config.h"
+
+#define SERVER_PORT 80
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
+
+int filledSlots = 0;
+
+WebServer server(SERVER_PORT);
+
+void handleRoot() {
+  String response = "Parking slots: " + String(filledSlots) + "/6";
+  server.send(200, "text/plain", response);
+}
 
 void setup() {
   // ESP32 as station
@@ -10,6 +22,7 @@ void setup() {
 
   // Start serial
   Serial.begin(115200);
+  while (!Serial) {;} // Wait for serial port to connect
 
   Serial.println();
   Serial.println("******************************************************");
@@ -28,9 +41,22 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
+
+  // Initialize API - Specify endpoint
+  server.on("/", handleRoot);
+
+  // Handle 404 (Not Found)
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "Not Found");
+  });
+
+  server.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient(); // Handle incoming client requests
+  filledSlots = random(0, 6+1);
+  delay(2000);
 
 }
